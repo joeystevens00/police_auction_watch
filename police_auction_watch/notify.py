08148371559fe656bd641109694ea3e1e5860ff0
Joey Stevens <joey.stevens00@gmail.com>
2021-10-27 21:19:08 -0500

    link in alert message

--- police_auction_watch/notify.py
+++ police_auction_watch/notify.py
@@ -10,7 +10,11 @@ def new_product(args: AuctionArgs, source: str, product: Dict):
     res = requests.post(
         config.slack_hook,
         json={
-            "text": f"New product ({args.slug}) listing on {source}: " + json.dumps(product),
+            "text": (
+                f"New product ({args.slug}) listing on {source}:"
+                f" \"{product['name']}\" {product['link']} price {product['last_price']}"
+                f" time remaining {product['time_remaining']}"
+            )
         }
     )
     print(res, res.text)
--- police_auction_watch/propertyroom.py
+++ police_auction_watch/propertyroom.py
@@ -34,8 +34,9 @@ def check_auctions(args: AuctionArgs):
         # Log the most recent item
         product_items = driver.find_elements(By.CSS_SELECTOR, '.section .product-boxes div')
         latest_item = product_items[0]
-        item_name = latest_item.find_element_by_css_selector('.product-name-category a').text
+        item_link = latest_item.find_element_by_css_selector('.product-name-category a')
+        item_name = item_link.text
         time_remaining = latest_item.find_element_by_css_selector('.time-bids-category ul li').text
         last_price = latest_item.find_element_by_css_selector('.time-bids-category ul li:nth-child(2)').text
 
-        return {"name": item_name, "time_remaining": time_remaining,  "last_price": last_price}
+        return {"name": item_name, "time_remaining": time_remaining,  "last_price": last_price, 'link': item_link.get_attribute('href')}
